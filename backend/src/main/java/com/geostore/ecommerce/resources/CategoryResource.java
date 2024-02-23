c4b4d0001317d8e3bf8f23dc27ccfc510d03e8db
Your Name <gfc.net@gmail.com>
2024-02-23 18:56:18 -0300

    DTO and database seeding

--- backend/src/main/java/com/geostore/ecommerce/resources/CategoryResource.java
+++ backend/src/main/java/com/geostore/ecommerce/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.geostore.ecommerce.entities.Category;
+import com.geostore.ecommerce.dto.CategoryDTO;
 import com.geostore.ecommerce.services.CategoryService;
 
 @RestController
@@ -19,8 +19,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
